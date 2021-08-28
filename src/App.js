2072dd6b15e18cadcdb3905751b16518bc41b14c
suarezt3 <ey_der19@hotmail.com>
2021-08-28 14:09:39 -0500

    Se realizaron pruebas con CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+//import React from 'react'
 import './App.css';
+import Header from './Components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      
+  <Header/>
     </div>
   );
 }
