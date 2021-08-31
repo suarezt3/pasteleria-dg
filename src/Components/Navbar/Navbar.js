11d167ab80a0220eb14168ae85699357fc86f6cf
suarezt3 <ey_der19@hotmail.com>
2021-08-30 20:53:50 -0500

    Se avanza en el menu de navegacion

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+
+  const [showLinks, setShowLinks] = useState(true);
+
   return (
     <div className="navbar">
       <div className="leftside"></div>
       <div className="rightside">
-        <div className="links">
+        <div className="links" id={showLinks ? "hidden": ""}>
           <a href="/Home">Inicio</a>
           <a href="/products">Productos</a>
           <a href="/About">Nostros</a>
           <a href="/contact">Contáctenos</a>
         </div>
+      <button onClick={()=> setShowLinks(!showLinks)} className="btn btn-menu">{" "}<ReorderIcon /></button>
       </div>
     </div>
   );
