373b804a2fe27e4f25b6b0675c4f048ac76ef9e0
suarezt3 <ey_der19@hotmail.com>
2021-09-03 12:43:55 -0500

    Se organiza las tarjeta

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,11 +7,14 @@ export const Card = () => {
       <div className="box-img">
         <img className="img" src="" alt="" />
       </div>
-      <p>Torta de Tres leches</p>
-      <div>
-          <label for="amount">Cantidad</label>
-        <input type="number" name="amount" min="0" max="20" step="1" />
+      <p className="product-description">Tortas temáticas (redondas o cuadradas) con Toppers en 3D</p>
+      <p className="size">500 g</p>
+      <div className="box-count">
+          <label className="amount" htmlFor="amount">Cantidad</label>
+        <input className="count" type="number" htmlname="amount" min="0" max="20" step="1" />
       </div>
+      <p className="price">$ 35.000</p>
+      <button className="btn-add">Agregar</button>
     </div>
   );
 };
