fc7beb0de86b4cee91afad29c29bf4ca680e61f7
suarezt3 <ey_der19@hotmail.com>
2021-09-07 16:36:53 -0500

    Se adelanta el JSON

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './Text.css';
-
+import React from "react";
+import "./Text.css";
 
 export const Text = () => {
-    return (
-        <div className="box-text">
-            <p className="text">* Hacemos cualquier cantidad que necesites desde 10 porciones en adelante. <br/>
-** Los precios varían dependiendo de los sabores, texturas, cantidad de pisos, complejidad y cantidad de decoraciones en 2 dimensiones que requiera el diseño. <br/>
-*** Las imágenes son de referencia y no corresponden a precio final del producto.</p>
-        </div>
-    )
-}
+  return (
+    <div className="box-text">
+      <p className="text">
+        * Hacemos cualquier cantidad que necesites desde 10 porciones en
+        adelante. <br />
+        ** Los precios varían dependiendo de los sabores, texturas, cantidad de
+        pisos, complejidad y cantidad de decoraciones en 2 dimensiones que
+        requiera el diseño. <br />
+        *** Las imágenes son de referencia y no corresponden a precio final del producto. <br />
+        **** Para los cupcakes tradicionales, el precio de la unidad varía según los sabores escogidos y el número total de unidades requeridas. Para los cupcakes personalizados el precio de la unidad varía según los sabores escogidos y la complejidad del diseño.
+      </p>
+    </div>
+  );
+};
