4292a3c85f0c49a7b74d3db4d5175ad0621fbd3d
suarezt3 <ey_der19@hotmail.com>
2021-09-08 15:17:38 -0500

    Se agrega el boton de pago

--- src/Components/ShoppingCart/shoppingCart.jsx
+++ src/Components/ShoppingCart/shoppingCart.jsx
@@ -21,6 +21,7 @@ export const ShoppingCart = () => {
             <h3 className="total">Total</h3>
             <h3 className="total-price">$ 35.000</h3>
         </div>
+        <button className="pay">Pagar</button>
         </div>
     )
 }
