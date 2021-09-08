2b6d18727e34a7f6ab3c00c9c8cfec5e5f2db6af
suarezt3 <ey_der19@hotmail.com>
2021-09-08 13:01:01 -0500

    Se termina de crear el JSON y se hacen retoques en la maquetacion

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -5,7 +5,9 @@ import "./Filter.css";
 export const Filter = () => {
     return (
         <ul className="filter">
-          
+          <li>Cakes</li>
+          <li>Cupcakes</li>
+          <li>Frozen</li>
         </ul>
     )
 }
