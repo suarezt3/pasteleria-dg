a08288807bbcd0257e1f66b62a1e8f714d5c5d17
suarezt3 <ey_der19@hotmail.com>
2021-09-11 09:50:36 -0500

    Se Reinstala el React Router

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Filter.css";
 
 export const Filter = () => {
   return (
     <ul className="filter">
-      <NavLink exact to="/products" activeClassName="active">Cakes</NavLink>
-      <NavLink exact to="/products2" activeClassName="active">Cupcakes</NavLink>
+      <Link exact to="/products" activeClassName="active">Cakes</Link>
+      <Link exact to="/products2" activeClassName="active">Cupcakes</Link>
     </ul>  
   );
 };
+
